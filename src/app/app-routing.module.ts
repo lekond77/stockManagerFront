34713c5547c6bd4b488efb063689251e3642a941
LEON AKONDE <jeu@jeu>
2024-10-29 23:44:04 +0100

    checkout to security

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
 import { ProductComponent } from "./product/product.component";
 import { HomeComponent } from "./home/home.component";
+import { SecurityComponent } from "./security/security.component";
 
 const routes: Routes = [
     {path : '', component: HomeComponent},
     {path : 'produits', component: ProductComponent},
-    //{path: 'produit', component:ProductComponent}
+    {path: 'login', component:SecurityComponent}
 ];
 
 @NgModule({
